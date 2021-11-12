5a2d58c1718c846b78be3244cac8960a821c36e4
Andrea <andreaclelan@gmail.com>
2021-11-12 10:42:33 -0800

    added auth to reviews

--- Routes/campgrounds.js
+++ Routes/campgrounds.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-const {campgroundSchema} = require('../schemas');
-const { isLoggedIn } = require('../middleware');
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error){
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
-    next();
-}
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+
 
 // All Campgrounds
 router.get('/', catchAsync(async (req, res) => {
@@ -28,8 +19,8 @@ router.get('/new', isLoggedIn, (req, res) => {
 
 // Post: New Campground
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    //if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success','Successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
@@ -37,7 +28,13 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 // Get: Show Page
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+        }).populate('author');
+    console.log(campground);
     if (!campground) {
         req.flash('error', 'Cannot find that campground');
         res.redirect('/campgrounds');
@@ -46,7 +43,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 }));
 
 // Get: Edit Campground
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
         req.flash('error', 'Cannot find that campground');
@@ -56,12 +53,13 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
 }));
 
 // Put: Edit Campground
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
-    req.flash('success', 'Campground successfully updated');
+    const campground = await Campground.findById(id);
+    const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    req.flash('success', 'Campground successfully updated'); 
     res.redirect(`/campgrounds/${campground._id}`)
-}))
+}));
 
 // Delete Campground
 router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,8 @@ const seedDB = async () => {
     location: "The Dark Wood",
     image: 'https://static.wikia.nocookie.net/bogm/images/0/04/Darkwoods.jpg',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, beatae illum. Facere illum odio mollitia, quo repudiandae ducimus molestiae similique tempore minima ut, earum delectus at, illo facilis esse distinctio.',
-    price: 0.00
+    price: 0.00,
+    author: '618c4bd25c9a435c2e51b0ff'
 })
     await c.save();
     for (let i =0; i < 50; i++){
@@ -40,7 +41,8 @@ const seedDB = async () => {
             location: `${city.city}, ${city.state}`,
             image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, beatae illum. Facere illum odio mollitia, quo repudiandae ducimus molestiae similique tempore minima ut, earum delectus at, illo facilis esse distinctio.',
-            price: price
+            price: price,
+            author: '618c4bd25c9a435c2e51b0ff'
         })
         await camp.save();
     }
